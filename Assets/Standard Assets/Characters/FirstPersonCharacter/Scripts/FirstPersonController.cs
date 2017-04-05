b62a9f74193c7b5e35ffc723e3c458a002b0fdbc
松田翔大 <matsudashota@matsudashoudainoMacBook-Pro.local>
2017-04-05 17:03:45 +0900

    Fixed FirstPersonController.cs

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 using UnityStandardAssets.Utility;
 using Random = UnityEngine.Random;
-using FPS_Controller = UnityStandardAssets.Characters.FirstPerson.FirstPersonController;
 
 namespace UnityStandardAssets.Characters.FirstPerson
 {
@@ -28,7 +27,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
         [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
         [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
-		[SerializeField] private float m_CrouchingSpeed;
 
         private Camera m_Camera;
         private bool m_Jump;
@@ -40,18 +38,17 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private bool m_PreviouslyGrounded;
         private Vector3 m_OriginalCameraPosition;
         private float m_StepCycle;
-        private float m_NextStep;
+		private float m_NextStep;
         private bool m_Jumping;
         private AudioSource m_AudioSource;
-		private bool m_crouching;
+        private bool m_crouching;
         // Use this for initialization
 
-		GameObject character;
-		float default_walk_speed, default_run_speed;
+        GameObject character;
+        float default_walk_speed, default_run_speed;
 
 
-        private void Start()
-        {
+        private void Start(){
             m_CharacterController = GetComponent<CharacterController>();
             m_Camera = Camera.main;
             m_OriginalCameraPosition = m_Camera.transform.localPosition;
@@ -69,10 +66,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 			
 
         // Update is called once per frame
-        private void Update()
-		{
-			
-
+        private void Update(){
 				if (Input.GetKey (KeyCode.C)) {
 					m_crouching = true;
 				} else {
